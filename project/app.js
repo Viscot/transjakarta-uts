38a9082a4411ed9ee13d3d80d536547806b61306
Hansen <hansenpratama947@gmail.com>
2024-04-26 00:40:01 +0700

    perbaikan dan penambahan mongodb

--- project/app.js
+++ project/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  secret: 'rahasia',    
+  secret: 'rahasia',
   resave: false,
   saveUninitialized: true
 }));
